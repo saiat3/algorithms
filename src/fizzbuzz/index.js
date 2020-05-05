6bf3fcd55943cc2b3355bf73391f7658081ee012
Saiat Kalbiev <kalbievich11@gmail.com>
2020-05-05 12:33:10 +0400

    Implement fizzbuzz algorithm

---
+++ src/fizzbuzz/index.js
@@ -0,0 +1,16 @@
+/**
+ * Problem:
+ * For each multiple of 3, print "fizz" instead of the number.
+ * For each multiple of 5, print "buzz" instead of the number.
+ * For numbers which are multiples of both 3 and 5, print "fizzbuzz" instead of the number.
+ *
+ * fizzbuzz(5)
+ * 1
+ * 2
+ * fizz
+ * 4
+ * buzz
+ */
+
+const fizzbuzz = require('./solution_1');
+fizzbuzz(15);
---
+++ src/fizzbuzz/solution_1.js
@@ -0,0 +1,13 @@
+module.exports = function fizzbuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else {
+      console.log(i);
+    }
+  }
+};
