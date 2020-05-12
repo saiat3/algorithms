1ea6bdfada8d536b83fa0a12b3e92dc88dc360b9
Saiat Kalbiev <kalbievich11@gmail.com>
2020-05-12 13:42:54 +0400

    Implement spiral matrix algorithm

---
+++ src/spiral-matrix/index.js
@@ -0,0 +1,23 @@
+/**
+ * Problem:
+ * Given positive integer N print out spiral matrix
+ *
+ * matrix(2) === [
+ *               [1,2]
+ *               [4,3]
+ *               ]
+ *
+ *
+ * matrix(4) === [
+ *                 [ 1, 2, 3, 4 ],
+ *                 [ 12, 13, 14, 5 ],
+ *                 [ 11, 16, 15, 6 ],
+ *                 [ 10, 9, 8, 7 ]
+ *               ]
+ *
+ *
+ */
+
+const solution_1 = require('./solution_1');
+
+console.log(solution_1(4));
