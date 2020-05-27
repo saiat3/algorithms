da8e0b0eb2e6dfa5a1a3e6a1a5ece8ec9bc44155
Saiat Kalbiev <kalbievich11@gmail.com>
2020-05-27 13:05:05 +0400

    Implement node class in linked list directory

---
+++ data-structures/linked-list/index.js
@@ -0,0 +1,11 @@
+/*
+ * A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations.
+ * In simple words, a linked list consists of nodes where each node contains a data field and a reference(link)
+ * to the next node in the list
+ *
+ * Problem:
+ * Implement a linked list data structure.
+ */
+
+const {LinkedList, Node} = require('./linked-list.js');
+
---
+++ data-structures/linked-list/linked-list.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+
+  }
+}
+
+module.exports = {Node, LinkedList};
