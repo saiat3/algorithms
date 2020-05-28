e88f22318951400c890916b37e097f7a00ca3232
Saiat Kalbiev <kalbievich11@gmail.com>
2020-05-28 14:41:54 +0400

    Implement node class for tree

---
+++ data-structures/tree/tree.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(node => node.data !== data);
+  }
+}
+
+
+module.exports = {Node};
